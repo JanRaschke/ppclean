f4737ec57b62070daf197559a5c586b9071939ea
JanRaschke <janphilipraschke@gmail.com>
2026-08-19 09:51:14 +0200

    Finalize Task 1-1, testing successful.

--- src/main/java/de/clean/similarity/SingleAttributeEquality.java
+++ src/main/java/de/clean/similarity/SingleAttributeEquality.java
@@ -1,16 +1,20 @@
 package de.clean.similarity;
 
+import java.util.List;
+
 import de.clean.data.Record;
+
 /**
- * Simple heuristic to compare two Records
- * It compares two Records for equality based only on a single attribute
+ * Simple heuristic to compare two Records It compares two Records for equality
+ * based only on a single attribute
  */
 public class SingleAttributeEquality implements RecordSimilarity {
 
     int attributeIndex;
 
     /**
-     * @param attributeIndex Position of Record content at which to check for equality
+     * @param attributeIndex Position of Record content at which to check for
+     * equality
      */
     public SingleAttributeEquality(int attributeIndex) {
         this.attributeIndex = attributeIndex;
@@ -19,15 +23,18 @@ public class SingleAttributeEquality implements RecordSimilarity {
     /**
      * @param r1
      * @param r2
-     * @return 1 if r1 and r2 are equal at position {@link #attributeIndex}, else 0
+     * @return 1 if r1 and r2 are equal at position {@link #attributeIndex},
+     * else 0
      */
     @Override
     public double compare(Record r1, Record r2) {
         double res = 0;
         // BEGIN SOLUTION
-
-
-
+        List<String> record1 = r1.getContent();
+        List<String> record2 = r2.getContent();
+        if (record1.get(attributeIndex).equals(record2.get(attributeIndex))) {
+            res = 1;
+        }
         // END SOLUTION
         return res;
     }
