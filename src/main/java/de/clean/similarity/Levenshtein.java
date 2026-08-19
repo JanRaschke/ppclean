36637f895bb02bd2bd29b3722951fdc031fc4427
JanRaschke <janphilipraschke@gmail.com>
2026-08-19 11:00:47 +0200

    First changes.

--- src/main/java/de/clean/similarity/Levenshtein.java
+++ src/main/java/de/clean/similarity/Levenshtein.java
@@ -4,11 +4,13 @@ package de.clean.similarity;
  * Levenshtein String similarity
  */
 public class Levenshtein implements StringSimilarity {
+
     public Levenshtein() {
     }
 
     /**
      * Calculates Levenshtein String similarity for x and y
+     *
      * @param x
      * @param y
      * @return Similarity score in range [0,1]
@@ -19,9 +21,33 @@ public class Levenshtein implements StringSimilarity {
         int m = x.length();
         int n = y.length();
         // BEGIN SOLUTION
+        int[][] matrix = new int[m + 1][n + 1];
+        //populate matrix with basic cases
+        for (int i = 0; i < m + 1; i++) {
+            matrix[i][0] = i;
+        }
+        for (int i = 0; i < n + 1; i++) {
+            matrix[0][i] = i;
+        }
+        for (int i = 0; i < m + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                if (x.charAt(i) == y.charAt(j)) {
+                    matrix[i][j] = matrix[i - 1][j - 1];
+                } else {
+                    matrix[i][j] = 1 + Math.min(compare(x.substring(0, m - 1), y.substring(0, n - 1)),)
+                }
 
-
-
+            }
+        }
+        //build visual table for understanding
+        String s = "";
+        for (int i = 0; i < m + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                s += matrix[i][j] + " ";
+            }
+            s += "\n";
+        }
+        System.out.println("hallo" + "\n" + s);
         // END SOLUTION
         return res;
     }
