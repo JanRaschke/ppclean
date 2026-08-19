2d88fb964e5d69317af6fe548236677e396435a3
JanRaschke <janphilipraschke@gmail.com>
2026-08-19 13:33:16 +0200

    Implement hybrid logic, testing successfull.

--- src/main/java/de/clean/similarity/Hybrid.java
+++ src/main/java/de/clean/similarity/Hybrid.java
@@ -1,10 +1,12 @@
 package de.clean.similarity;
 
 import java.util.List;
+
 import de.clean.data.Record;
 
 /**
- * Record similarity for comparing two Records attribute by attribute either with {@link Levenshtein} or {@link Jaccard}
+ * Record similarity for comparing two Records attribute by attribute either
+ * with {@link Levenshtein} or {@link Jaccard}
  */
 public class Hybrid implements RecordSimilarity {
 
@@ -14,9 +16,10 @@ public class Hybrid implements RecordSimilarity {
     private StringSimilarity levenshtein;
 
     /**
-     * @param policies List of comparison policies, write "L" for {@link Levenshtein},
-     *                 "J" {@link Jaccard}, and null to skip an attribute.
-     *                 The policies are applied in order of attributes (e.g., first policy to first attribute)
+     * @param policies List of comparison policies, write "L" for
+     * {@link Levenshtein}, "J" {@link Jaccard}, and null to skip an attribute.
+     * The policies are applied in order of attributes (e.g., first policy to
+     * first attribute)
      */
     public Hybrid(List<String> policies) {
         this.policies = policies;
@@ -25,8 +28,10 @@ public class Hybrid implements RecordSimilarity {
     }
 
     /**
-     * Compares two Records attribute by attribute according to {@link #policies}.
-     * For Jaccard similarity, a default window size of {@link #JACCARD_N} is used
+     * Compares two Records attribute by attribute according to
+     * {@link #policies}. For Jaccard similarity, a default window size of
+     * {@link #JACCARD_N} is used
+     *
      * @param r1
      * @param r2
      * @return Similarity score in range [0,1] (1=same, 0=very different)
@@ -35,11 +40,34 @@ public class Hybrid implements RecordSimilarity {
     public double compare(Record r1, Record r2) {
         double res = 0;
         // BEGIN SOLUTION
+        List<String> record1 = r1.getContent();
+        List<String> record2 = r2.getContent();
 
+        //calc number of attr
+        int n = record1.size();
 
+        //sum of algorith results
+        double sum = 0.0;
+        //number of used algorithms
+        int count = 0;
 
+        for (int i = 0; i < n; i++) {
+            String policy = policies.get(i);
+
+            if (policy == null) {
+                continue;
+            } else if (policy.equals("L")) {
+                sum += levenshtein.compare(record1.get(i), record2.get(i));
+                count++;
+            } else if (policy.equals("J")) {
+                sum += jaccard.compare(record1.get(i), record2.get(i));
+                count++;
+            }
+
+        }
+
+        res = sum / count;
         // END SOLUTION
         return res;
     }
 }
-
